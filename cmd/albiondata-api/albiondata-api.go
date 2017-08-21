15cc84c975e5abc22e6e0727211fc52670beb4ae
Rene Jochum <rene@jochums.at>
2017-08-21 19:26:04 +0200

    Add the actual code and fix .gitignore
    Signed-off-by: Rene Jochum <rene@jochums.at>

---
+++ cmd/albiondata-api/albiondata-api.go
@@ -0,0 +1,166 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+
+	"github.com/pcdummy/albiondata-api/lib"
+	adslib "github.com/pcdummy/albiondata-sql/lib"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	homedir "github.com/mitchellh/go-homedir"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var (
+	version string
+	cfgFile string
+	db      *gorm.DB
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "albiondata-api",
+	Short: "albiondata-api is the API Server for the Albion Data Project",
+	Long: `Reads data from a SQL Database (MySQL, PostgreSQL and SQLite3 are supported), 
+and serves them through a HTTP API`,
+	Run: doCmd,
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.albiondata-api.yaml")
+	rootCmd.PersistentFlags().StringP("listen", "l", "[::1]:3080", "Host and port to listen on")
+	rootCmd.PersistentFlags().StringP("dbType", "t", "mysql", "Database type must be one of mysql, postgresql, sqlite3")
+	rootCmd.PersistentFlags().StringP("dbURI", "u", "", "Databse URI to connect to, see: http://jinzhu.me/gorm/database.html#connecting-to-a-database")
+	viper.BindPFlag("listen", rootCmd.PersistentFlags().Lookup("listen"))
+	viper.BindPFlag("dbType", rootCmd.PersistentFlags().Lookup("dbType"))
+	viper.BindPFlag("dbURI", rootCmd.PersistentFlags().Lookup("dbURI"))
+}
+
+func initConfig() {
+	// Don't forget to read config either from cfgFile or from home directory!
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		viper.AddConfigPath("/etc")
+
+		// Find home directory.
+		home, err := homedir.Dir()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		// Search config in home directory with name ".cobra" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigName("albiondata-api")
+
+		wd, _ := os.Getwd()
+		viper.AddConfigPath(wd)
+	}
+
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Println("Can't read config:", err)
+	}
+
+	viper.SetEnvPrefix("ADA")
+	viper.AutomaticEnv()
+}
+
+func apiHome(c echo.Context) error {
+	return c.String(http.StatusOK, "Nothing to show here, please go to https://www.albiononline2d.com/")
+}
+
+func apiHandleStatsPricesItem(c echo.Context) error {
+	result := []lib.APIStatsPricesItem{}
+
+	for _, l := range adslib.Locations() {
+		lres := lib.APIStatsPricesItem{
+			City: l.String(),
+		}
+		m := l.Model()
+
+		// Find lowest offer price
+		if err := db.Where("item_id = ? and auction_type = ?", c.Param("item"), "offer").Order("price").First(m).Error; err != nil {
+			continue
+		}
+		lres.SellPriceMin = m.Price
+
+		// Find highest offer price
+		m = l.Model()
+		if err := db.Where("item_id = ? and auction_type = ?", c.Param("item"), "offer").Order("price desc").First(m).Error; err == nil {
+			lres.SellPriceMax = m.Price
+		}
+
+		// Find lowest request price
+		m = l.Model()
+		if err := db.Where("item_id = ? and auction_type = ?", c.Param("item"), "request").Order("price").First(m).Error; err == nil {
+			lres.BuyPriceMin = m.Price
+		}
+
+		// Find highest request price
+		m = l.Model()
+		if err := db.Where("item_id = ? and auction_type = ?", c.Param("item"), "request").Order("price desc").First(m).Error; err == nil {
+			lres.BuyPriceMax = m.Price
+		}
+
+		result = append(result, lres)
+	}
+
+	return c.JSON(http.StatusOK, result)
+}
+
+func doCmd(cmd *cobra.Command, args []string) {
+	//******************************
+	// START DB
+	fmt.Printf("Connecting to database: %s\n", viper.GetString("dbType"))
+	var err error
+	db, err = gorm.Open(viper.GetString("dbType"), viper.GetString("dbURI"))
+	if err != nil {
+		fmt.Printf("%v\n", err)
+		return
+	}
+
+	// Debug
+	// db.LogMode(true)
+
+	defer db.Close()
+	// END DB
+	//******************************
+
+	//******************************
+	// START ECHO
+	e := echo.New()
+	e.HideBanner = true
+
+	// Recover from panics
+	e.Use(middleware.Recover())
+
+	// Logger
+	e.Use(middleware.Logger())
+
+	e.GET("/", apiHome)
+	e.GET("/api/v1/stats/prices/:item", apiHandleStatsPricesItem)
+
+	// Start server
+	e.Logger.Fatal(e.Start(viper.GetString("listen")))
+
+	// END ECHO
+	//*******************************
+}
+
+func main() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
